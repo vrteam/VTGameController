57519efa7e03e53d2f90d8bb78cd14fc8cfe1631
Qin Hong <dev@vr2d.cn>
2016-03-15 23:12:45 +0800

    Add empty project

---
+++ VTGameController/VTGameController.h
@@ -0,0 +1,19 @@
+//
+//  VTGameController.h
+//  VTGameController
+//
+//  Created by mcmillen on 16/3/15.
+//  Copyright © 2016年 vr2d.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTGameController.
+FOUNDATION_EXPORT double VTGameControllerVersionNumber;
+
+//! Project version string for VTGameController.
+FOUNDATION_EXPORT const unsigned char VTGameControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTGameController/PublicHeader.h>
+
+
